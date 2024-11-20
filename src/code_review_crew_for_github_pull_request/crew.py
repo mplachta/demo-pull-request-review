6c6cdb3ea1adae580bd446f3053c76f82a289e37
João Moura <joaomdmoura@gmail.com>
2024-11-20 14:33:28 -0300

    updating github tools

--- src/code_review_crew_for_github_pull_request/crew.py
+++ src/code_review_crew_for_github_pull_request/crew.py
@@ -1,46 +1,71 @@
 from crewai import Agent, Crew, Process, Task
-from crewai.project import CrewBase, agent, crew, task
+from crewai.project import CrewBase, agent, crew, task, before_crew
 from crewai_tools import ScrapeWebsiteTool
-import os
+from .tools.github_tools import (
+    FetchPRFilesTool,
+    FindRelatedPRsTool,
+    FindRelatedFilesTool,
+)
 
 @CrewBase
 class CodeReviewCrewForGithubPullRequestCrew():
     """CodeReviewCrewForGithubPullRequest crew"""
 
+    @before_crew
+    def load_files_changed(self, inputs):
+        self.files_changed = FetchPRFilesTool().run(inputs['pr_patch_url'])
+        inputs['files_changed'] = self.files_changed
+        return inputs
+
     @agent
-    def code_retriever(self) -> Agent:
+    def quick_review_assistant(self) -> Agent:
         return Agent(
-            config=self.agents_config['code_retriever'],
+            config=self.agents_config['quick_review_assistant'],
+            tools=[
+                ScrapeWebsiteTool(),
+                FetchPRFilesTool(),
+                FindRelatedPRsTool(),
+                FindRelatedFilesTool(),
+            ]
         )
 
     @agent
     def code_review_expert(self) -> Agent:
         return Agent(
-            config=self.agents_config['code_review_expert'],
-        )
-
-
-    @task
-    def retrieve_modified_files_task(self) -> Task:
-        return Task(
-            config=self.tasks_config['retrieve_modified_files_task'],
-            tools=[ScrapeWebsiteTool()],
+            config=self.agents_config['code_review_expert']
         )
 
     @task
     def analyze_code_changes_task(self) -> Task:
         return Task(
             config=self.tasks_config['analyze_code_changes_task'],
-            tools=[],
+            tools=[]
         )
 
+    @task
+    def find_related_prs_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['find_related_prs_task']
+        )
+
+    @task
+    def analyze_related_files_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['analyze_related_files_task']
+        )
+
+    @task
+    def generate_review_comment_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['generate_review_comment_task']
+        )
 
     @crew
     def crew(self) -> Crew:
         """Creates the CodeReviewCrewForGithubPullRequest crew"""
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
+            agents=self.agents,
+            tasks=self.tasks,
             process=Process.sequential,
             verbose=True,
         )
